fadb5f4501edfac09a7db5e88c05fdc58c0277a5
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-08-31 12:09:44 +0600

    implement full star rating

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Stars from './Stars'
 
 const Ratings = () => {
+    const [rating, setRating] = useState(0)
   return (
     <div className='flex items-center justify-center min-h-screen'>
         <div className='bg-[#f5ffbe] rounded-xl shadow-xl text-center p-6 sm:p-10 md:p-14 lg:p-18'>
         <h2 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-gray-600 mb-6 sm:mb-8'>Ratings</h2> 
-        <div className='flex justify-center mb-4 gap-1 sm:gap-2'> <Stars /> </div>
+        <div className='flex justify-center mb-4 gap-1 sm:gap-2'>
+             {Array.from({ length: 5 },(_, index) => (
+                <Stars key={index} index={index} rating={rating} setRating={setRating} />
+             ))}
+             </div>
+             <p className='text-gray-600'>{rating} out of 5</p>
         </div>
     </div>
   )
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import { FaRegStar, FaStar } from 'react-icons/fa'
 
-
-const Stars = () => {
-  return (
-    <div>
-    <div className='flex justify-center items-center gap-2'>
-        <FaStar className='w-10 h-10 text-yellow-500' />
-        <FaStar className='w-10 h-10 text-yellow-500' />
-        <FaStar className='w-10 h-10 text-yellow-500' />
-        <FaStar className='w-10 h-10 text-yellow-500' />
-        <FaStar className='w-10 h-10 text-yellow-500' />
-    </div>
-    <p>5 out of 5</p>
-    </div>
-  )
+const Stars = ({rating, setRating, index}) => {
+    const handleRating = () => {
+        setRating(index + 1)
+    }
+    
+    return (
+        <div>
+            <i className={`bx ${rating >= index + 1 ? 'bxs-star' : 'bx-star'} text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-yellow-500`} onClick={handleRating}></i>
+        </div>
+    )
 }
 
 export default Stars
