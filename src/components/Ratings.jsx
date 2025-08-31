066af4debef01162ca97627a22a6ec9fea5b902a
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-08-31 12:40:41 +0600

    implement half full and empty star functionalit

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -3,13 +3,18 @@ import Stars from './Stars'
 
 const Ratings = () => {
     const [rating, setRating] = useState(0)
+
+    const handleRating = (starIndex, isLeftHalf) => {
+        const newRating = isLeftHalf ? starIndex + 0.5 : starIndex + 1
+        setRating(newRating)
+    }
   return (
     <div className='flex items-center justify-center min-h-screen'>
         <div className='bg-[#f5ffbe] rounded-xl shadow-xl text-center p-6 sm:p-10 md:p-14 lg:p-18'>
         <h2 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-gray-600 mb-6 sm:mb-8'>Ratings</h2> 
         <div className='flex justify-center mb-4 gap-1 sm:gap-2'>
              {Array.from({ length: 5 },(_, index) => (
-                <Stars key={index} index={index} rating={rating} setRating={setRating} />
+                <Stars key={index} index={index} rating={rating} onRatingChange={handleRating} />
              ))}
              </div>
              <p className='text-gray-600'>{rating} out of 5</p>
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,13 +1,18 @@
+'use client'
 import React from 'react'
 
-const Stars = ({rating, setRating, index}) => {
-    const handleRating = () => {
-        setRating(index + 1)
-    }
+const Stars = ({rating, index, onRatingChange}) => {
+    const handleLeftClick = () => onRatingChange(index, true)
+    const handleRightClick = () => onRatingChange(index, false)
     
+    const isFullStar = rating >= index + 1
+    const isHalfStar = rating === index + 0.5 && rating < index + 1
+    const starClass = isFullStar ? 'bxs-star' : isHalfStar ? 'bxs-star-half' : 'bx-star'
     return (
-        <div>
-            <i className={`bx ${rating >= index + 1 ? 'bxs-star' : 'bx-star'} text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-yellow-500`} onClick={handleRating}></i>
+        <div className='relative'>
+            <i className={`bx ${starClass} text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-yellow-500`}></i>
+            <button className='absolute top-0 left-0 w-1/2 h-full cursor-pointer' onClick={handleLeftClick}/>
+            <button className='absolute top-0 right-0 w-1/2 h-full cursor-pointer' onClick={handleRightClick}/>
         </div>
     )
 }
